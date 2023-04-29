9f3b158a5ea0e1f1ea2adb3eb56c9f0b57965094
iOS Lab <iOS Lab@Macbook-Air-21.local>
2023-04-29 12:59:08 -0600

    Creación del Proyecto

---
+++ iShare/iShare/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  iShare
+//
+//  Created by iOS Lab on 29/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack
+        {
+            Text("App iShare")
+        }
+
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iShare/iShare/iShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  iShareApp.swift
+//  iShare
+//
+//  Created by iOS Lab on 29/04/23.
+//
+
+import SwiftUI
+
+@main
+struct iShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
